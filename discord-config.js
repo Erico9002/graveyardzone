6ac5403dafc8aa4fcd06eff7d51ea2ef9292a2f8
Erico9002 <57814950+Erico9002@users.noreply.github.com>
2022-07-10 00:15:18 -0500

    Update discord-config.js

--- discord-config.js
+++ discord-config.js
@@ -23,7 +23,7 @@ const config = convict({
             id: {
                 doc: "The Discord server ID you want to simulate.",
                 format: String,
-                default: "123456789"
+                default: "182169442162049024"
             },
             default: {
                 doc: "Indicates whether clients connect to this server by default. One server should have this set to true.",
